b1953e5eefd81953f3ec114315a77cd68b563f78
pankajk86 <kumar.pankaj.it@gmail.com>
2017-07-23 15:20:37 -0700

    first commit

---
+++ src/main/java/org/personal/migration/Cassandra2Hadoop/App.java
@@ -0,0 +1,12 @@
+package org.personal.migration.Cassandra2Hadoop;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); 
+        
+        
+    }
+}
